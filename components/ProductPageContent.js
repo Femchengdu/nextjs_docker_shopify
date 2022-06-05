c5f8e9952c80b36208695b1c1bcd9a2f3707c7e1
Samuel Ajayi <femiajayi705@gmail.com>
2022-06-05 21:36:06 +0200

    Added recommend products

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import ProductForm from './ProductForm'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Navigation, Pagination } from 'swiper'
+import RecommendedList from './RecommendedList'
 
 export default function ProductPageContent({ product }) {
     const images = []
@@ -16,21 +17,26 @@ export default function ProductPageContent({ product }) {
 
     SwiperCore.use([Navigation, Pagination])
     return (
-        <div className='flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto'>
-            <div className="w-full max-w-md border bg-white rounded">
-                <div className="relative h-96 w-full">
-                    <Swiper
-                        style={{ '--swiper-navigation-color': '#000', '--swiper-pagination-color': '#000' }}
-                        navigation
-                        pagination={{ clickable: true }}
-                        className="h-96 rounded-2xl"
-                        loop="ture"
-                    >
-                        {images}
-                    </Swiper>
+        <div>
+
+            <div className='flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto'>
+                <div className="w-full max-w-md border bg-white rounded">
+                    <div className="relative h-96 w-full">
+                        <Swiper
+                            style={{ '--swiper-navigation-color': '#000', '--swiper-pagination-color': '#000' }}
+                            navigation
+                            pagination={{ clickable: true }}
+                            className="h-96 rounded-2xl"
+                            loop="ture"
+                        >
+                            {images}
+                        </Swiper>
+                    </div>
                 </div>
+                <ProductForm product={product} />
             </div>
-            <ProductForm product={product} />
+
+            <RecommendedList current={product.id} products={product.collections.edges[0].node.products.edges} />
         </div>
     )
 }
